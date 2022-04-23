380c88102fae59e07ee36f71a526da5cb00c3f3e
Piotr Podgorski <p.t.podgorski@gmail.com>
2022-04-23 14:48:33 +0200

    item details component in progres

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 function ItemDetail(props) {
   const { id } = useSelector((state) => state.actualItem);
@@ -34,7 +35,53 @@ function ItemDetail(props) {
           <h2>{item && item.name}</h2>
           <p>{item && item.description}</p>
           <ul>
+            <li>
+              Volume: {item && `${item.volume.value} ${item.volume.unit}`}
+            </li>
+            <li>Food pairing: {item && item.food_pairing}</li>
+            <li>Brewer's tips: {item && item.brewers_tips}</li>
             <li>first brewed: {item && item.first_brewed}</li>
+            <li>
+              <h6>Ingredients:</h6>
+              <ul>
+                <li>
+                  Malt:
+                  <ul>
+                    {item &&
+                      item.ingredients.malt.map((malt) => (
+                        <li
+                          key={malt.name}
+                        >{`${malt.name} - ${malt.amount.value} ${malt.amount.unit}`}</li>
+                      ))}
+                  </ul>
+                </li>
+                <li>
+                  Hops:
+                  <ul>
+                    {item &&
+                      item.ingredients.hops.map((hop) => (
+                        <li
+                          key={hop.name}
+                        >{`${hop.name} - ${hop.amount.value} ${hop.amount.unit}`}</li>
+                      ))}
+                  </ul>
+                </li>
+                <li>Yeast: {item && ` ${item.ingredients.yeast}`}</li>
+              </ul>
+            </li>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={
+                <Tooltip id="button-tooltip-2">Check out this avatar</Tooltip>
+              }
+            >
+              {({ ref, ...triggerHandler }) => <li>IBU: {item && item.ibu}</li>}
+            </OverlayTrigger>
+
+            <li>SRM: {item && item.srm}</li>
+            <li>Attenuation level: {item && item.attenuation_level}</li>
+            <li>Tagline: {item && item.tagline}</li>
+            <li>contributed by: {item && item.contributed_by}</li>
             <li>item id: {item && id}</li>
           </ul>
         </div>
