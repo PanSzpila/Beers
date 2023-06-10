016535450539c28776f5930346048da8d0b22826
Piotr Podgórski <p.t.podgorski+git@gmail.com>
2023-06-11 00:07:40 +0200

    r08 fix - index.js must not be jsx

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./custom.scss";
 
 ReactDOM.render(
   <StrictMode>
-      <App />
+    <App />
   </StrictMode>,
   document.getElementById("root") || document.createElement("div") // for testing
 );
